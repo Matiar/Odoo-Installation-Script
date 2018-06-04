c74616f6501f74a3d6247520aaf2dd836dbd6505
root <root@ip-172-16-250-40.ap-southeast-1.compute.internal>
2018-06-04 07:22:12 +0000

    ..

--- odoo_install.sh
+++ odoo_install.sh
@@ -108,7 +108,7 @@ sudo chown $OE_USER:$OE_USER /var/log/$OE_USER
 # Install ODOO
 #--------------------------------------------------
 echo -e "\n==== Installing ODOO Server ===="
-#sudo git clone --depth 1 --branch $OE_VERSION https://www.github.com/odoo/odoo $OE_HOME_EXT/
+sudo git clone --depth 1 --branch $OE_VERSION https://www.github.com/odoo/odoo $OE_HOME_EXT/
 sudo pip install -r $OE_HOME_EXT/requirements.txt
 
 if [ $IS_ENTERPRISE = "True" ]; then
