1eec7e55e43aa1b90ef8a4bcc1d002c0dc311f17
Matiar Rahman <matiar.rahman@gmail.com>
2021-06-28 20:38:49 +0600

    Update...

--- oca_install.sh
+++ oca_install.sh
@@ -32,7 +32,7 @@ IS_ENTERPRISE="False"
 #set the superadmin password
 OE_SUPERADMIN="admin"
 OE_CONFIG="${OE_USER}-server"
-OE_OCA_HOME="$OE_HOME/custom"
+OE_OCA_HOME="$OE_HOME/custom/12.0"
 
 
 #--------------------------------------------------
@@ -43,48 +43,42 @@ echo -e "\n---- Create custom module directory ----"
 sudo su $OE_USER -c "mkdir $OE_HOME/custom"
 
 echo -e "\n==== Downloan OCA Repository and Install Dependencies ===="
-sudo git clone --depth 1 --branch $OE_VERSION https://github.com/OCA/web.git $OE_OCA_HOME/web/
-
-sudo git clone --depth 1 --branch $OE_VERSION https://github.com/OCA/server-tools.git $OE_OCA_HOME/server-tools/
-sudo pip install -r $OE_OCA_HOME/server-tools/requirements.txt
-
-sudo git clone --depth 1 --branch $OE_VERSION https://github.com/OCA/operating-unit.git $OE_OCA_HOME/operating-unit/
-sudo git clone --depth 1 --branch $OE_VERSION https://github.com/OCA/reporting-engine.git $OE_OCA_HOME/reporting-engine/
-sudo git clone --depth 1 --branch $OE_VERSION https://github.com/OCA/crm.git $OE_OCA_HOME/crm/
-sudo git clone --depth 1 --branch $OE_VERSION https://github.com/OCA/hr.git $OE_OCA_HOME/hr/
 
 sudo git clone --depth 1 --branch $OE_VERSION https://github.com/OCA/account-financial-tools.git $OE_OCA_HOME/account-financial-tools/
 sudo git clone --depth 1 --branch $OE_VERSION https://github.com/OCA/account-financial-reporting.git $OE_OCA_HOME/account-financial-reporting/
 sudo git clone --depth 1 --branch $OE_VERSION https://github.com/OCA/account-invoicing.git $OE_OCA_HOME/account-invoicing/
 sudo git clone --depth 1 --branch $OE_VERSION https://github.com/OCA/account-closing.git $OE_OCA_HOME/account-closing/
 sudo git clone --depth 1 --branch $OE_VERSION https://github.com/OCA/account-analytic.git $OE_OCA_HOME/account-analytic/
-sudo git clone --depth 1 --branch $OE_VERSION https://github.com/OCA/product-attribute.git $OE_OCA_HOME/product-attribute/
 
-sudo git clone --depth 1 --branch $OE_VERSION https://github.com/OCA/partner-contact.git $OE_OCA_HOME/partner-contact/
-sudo git clone --depth 1 --branch $OE_VERSION https://github.com/OCA/purchase-workflow.git $OE_OCA_HOME/purchase-workflow/
-sudo git clone --depth 1 --branch $OE_VERSION https://github.com/OCA/reporting-engine.git $OE_OCA_HOME/reporting-engine/
-#sudo git clone --depth 1 --branch $OE_VERSION https://github.com/modoolar/project_agile.git $OE_OCA_HOME/project_agile/
-sudo git clone --depth 1 --branch $OE_VERSION https://github.com/OCA/sale-workflow.git $OE_OCA_HOME/sale-workflow/
+sudo git clone --depth 1 --branch $OE_VERSION https://github.com/OCA/account-analytic.git $OE_OCA_HOME/bank-payment/
+sudo git clone --depth 1 --branch $OE_VERSION https://github.com/OCA/crm.git $OE_OCA_HOME/crm/
+sudo git clone --depth 1 --branch $OE_VERSION https://github.com/OCA/hr.git $OE_OCA_HOME/hr/
+
+sudo git clone --depth 1 --branch $OE_VERSION https://github.com/OCA/operating-unit.git $OE_OCA_HOME/operating-unit/
 sudo git clone --depth 1 --branch $OE_VERSION https://bitbucket.org/matiarrahman/odoo-community-addons.git $OE_OCA_HOME/odoo-community-addons/
-sudo git clone --depth 1 --branch $OE_VERSION https://github.com/OCA/intrastat.git $OE_OCA_HOME/intrastat/
+sudo pip3 install -r $OE_OCA_HOME/odoo-community-addons/requirements.txt
 
-#sudo pip install -r $OE_OCA_HOME/project_agile/requirements.txt
-echo -e "\n === Installing requirements for partner-contact ==="
-sudo pip install -r $OE_OCA_HOME/partner-contact/requirements.txt
-echo -e "\n === Installing requirements for odoo-community-addons ==="
-sudo pip install -r $OE_OCA_HOME/odoo-community-addons/requirements.txt
+sudo git clone --depth 1 --branch $OE_VERSION https://github.com/OCA/product-attribute.git $OE_OCA_HOME/product-attribute/
+sudo git clone --depth 1 --branch $OE_VERSION https://github.com/OCA/partner-contact.git $OE_OCA_HOME/partner-contact/
+sudo pip3 install -r $OE_OCA_HOME/partner-contact/requirements.txt
+sudo git clone --depth 1 --branch $OE_VERSION https://github.com/OCA/purchase-workflow.git $OE_OCA_HOME/project/
+sudo git clone --depth 1 --branch $OE_VERSION https://github.com/OCA/purchase-workflow.git $OE_OCA_HOME/purchase-workflow/
 
-sudo pip install validate_email pymongo
+sudo git clone --depth 1 --branch $OE_VERSION https://github.com/OCA/reporting-engine.git $OE_OCA_HOME/reporting-engine/
 
-echo -e "\n==== Downloan GBS Repository and Install Dependencies ===="
-#sudo git clone --depth 1 --branch $OE_VERSION https://github.com/genweb2/gbs.git $OE_OCA_HOME/addons/
-sudo git clone --depth 1 --branch $OE_VERSION https://gitlab.com/genweb2ltd/gbs-account.git $OE_OCA_HOME/gbs-account/
+sudo git clone --depth 1 --branch $OE_VERSION https://github.com/OCA/sale-workflow.git $OE_OCA_HOME/sale-workflow/
+sudo git clone --depth 1 --branch $OE_VERSION https://github.com/OCA/server-tools.git $OE_OCA_HOME/server-tools/
+sudo pip3 install -r $OE_OCA_HOME/server-tools/requirements.txt
+sudo git clone --depth 1 --branch $OE_VERSION https://github.com/OCA/server-tools.git $OE_OCA_HOME/server-ux/
+
+sudo git clone --depth 1 --branch $OE_VERSION https://github.com/OCA/web.git $OE_OCA_HOME/web/
+
+sudo pip3 install validate_email pymongo
 
 echo -e "\n---- Setting permissions on home folder ----"
 sudo chown -R $OE_USER:$OE_USER $OE_HOME/*
 
 sudo su root -c "echo 'addons_path=/odoo/odoo-server/addons,/odoo/odoo-server/odoo/addons,$OE_OCA_HOME/web,$OE_OCA_HOME/server-tools,$OE_OCA_HOME/operating-unit,$OE_OCA_HOME/partner-contact,$OE_OCA_HOME/hr,$OE_OCA_HOME/project_agile,$OE_OCA_HOME/reporting-engine,$OE_OCA_HOME/purchase-workflow,$OE_OCA_HOME/sale-workflow,$OE_OCA_HOME/odoo-community-addons,$OE_OCA_HOME/intrastat,$OE_OCA_HOME/addons' >> /etc/${OE_CONFIG}.conf"
-
 
 echo "-----------------------------------------------------------"
 echo "Done! "
