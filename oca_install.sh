eea32002e2ac98f14c45cc0927f5aa4fc808fd26
Matiar <matiar.rahman@gmail.com>
2022-08-04 14:54:22 +0600

    ...

--- oca_install.sh
+++ oca_install.sh
@@ -26,7 +26,7 @@ INSTALL_WKHTMLTOPDF="True"
 OE_PORT="8010"
 #Choose the Odoo version which you want to install. For example: 10.0, 9.0, 8.0, 7.0 or saas-6. When using 'trunk' the master version will be installed.
 #IMPORTANT! This script contains extra libraries that are specifically needed for Odoo 10.0
-OE_VERSION="10.0"
+OE_VERSION="15.0"
 # Set this to True if you want to install Odoo 10 Enterprise!
 IS_ENTERPRISE="False"
 #set the superadmin password
@@ -62,28 +62,18 @@ sudo git clone --depth 1 --branch $OE_VERSION https://github.com/OCA/product-att
 
 sudo git clone --depth 1 --branch $OE_VERSION https://github.com/OCA/partner-contact.git $OE_OCA_HOME/partner-contact/
 sudo git clone --depth 1 --branch $OE_VERSION https://github.com/OCA/purchase-workflow.git $OE_OCA_HOME/purchase-workflow/
-sudo git clone --depth 1 --branch $OE_VERSION https://github.com/OCA/reporting-engine.git $OE_OCA_HOME/reporting-engine/
 #sudo git clone --depth 1 --branch $OE_VERSION https://github.com/modoolar/project_agile.git $OE_OCA_HOME/project_agile/
 sudo git clone --depth 1 --branch $OE_VERSION https://github.com/OCA/sale-workflow.git $OE_OCA_HOME/sale-workflow/
 sudo git clone --depth 1 --branch $OE_VERSION https://bitbucket.org/matiarrahman/odoo-community-addons.git $OE_OCA_HOME/odoo-community-addons/
 sudo git clone --depth 1 --branch $OE_VERSION https://github.com/OCA/intrastat.git $OE_OCA_HOME/intrastat/
 
-#sudo pip install -r $OE_OCA_HOME/project_agile/requirements.txt
-echo -e "\n === Installing requirements for partner-contact ==="
-sudo pip install -r $OE_OCA_HOME/partner-contact/requirements.txt
-echo -e "\n === Installing requirements for odoo-community-addons ==="
-sudo pip install -r $OE_OCA_HOME/odoo-community-addons/requirements.txt
-
+echo -e "\n === sudo pip install validate_email pymongo"
 sudo pip install validate_email pymongo
-
-echo -e "\n==== Downloan GBS Repository and Install Dependencies ===="
-#sudo git clone --depth 1 --branch $OE_VERSION https://github.com/genweb2/gbs.git $OE_OCA_HOME/addons/
-sudo git clone --depth 1 --branch $OE_VERSION https://gitlab.com/genweb2ltd/gbs-account.git $OE_OCA_HOME/gbs-account/
 
 echo -e "\n---- Setting permissions on home folder ----"
 sudo chown -R $OE_USER:$OE_USER $OE_HOME/*
 
-sudo su root -c "echo 'addons_path=/odoo/odoo-server/addons,/odoo/odoo-server/odoo/addons,$OE_OCA_HOME/web,$OE_OCA_HOME/server-tools,$OE_OCA_HOME/operating-unit,$OE_OCA_HOME/partner-contact,$OE_OCA_HOME/hr,$OE_OCA_HOME/project_agile,$OE_OCA_HOME/reporting-engine,$OE_OCA_HOME/purchase-workflow,$OE_OCA_HOME/sale-workflow,$OE_OCA_HOME/odoo-community-addons,$OE_OCA_HOME/intrastat,$OE_OCA_HOME/addons' >> /etc/${OE_CONFIG}.conf"
+sudo su root -c "echo 'addons_path=/odoo/odoo-server/addons,/odoo/odoo-server/odoo/addons,$OE_OCA_HOME/web,$OE_OCA_HOME/server-tools,$OE_OCA_HOME/operating-unit,$OE_OCA_HOME/partner-contact,$OE_OCA_HOME/hr,$OE_OCA_HOME/project_agile,$OE_OCA_HOME/reporting-engine,$OE_OCA_HOME/purchase-workflow,$OE_OCA_HOME/sale-workflow,$OE_OCA_HOME/odoo-community-addons,$OE_OCA_HOME/intrastat' >> /etc/${OE_CONFIG}.conf"
 
 
 echo "-----------------------------------------------------------"
